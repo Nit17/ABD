6c27b85445d837f9e670b417348f63b595803f68
Nithin B M <141205274+Nit17@users.noreply.github.com>
2023-08-09 16:02:55 +0530

    Add files via upload

---
+++ nitin.sql
@@ -0,0 +1,40 @@
+create Table Student (
+regNumber INT PRIMARY KEY,
+Name Varchar(100),
+email Varchar(150),
+phone varchar(20)
+);
+create Table Instructor (
+EmpID INT PRIMARY KEY,
+Name Varchar(100),
+email Varchar(150),
+Designation varchar(100),
+phone Varchar(20)
+);
+create Table Course (
+CouseID Int PRIMARY KEY,
+Name Varchar (100),
+contactHours int,
+InstructorID int,
+Foreign Key (InstructorID) refrences Instructor(EmpID)
+);
+Create table Take(
+StudentID int,
+courseID int,
+Grade Varchar(2),
+Primary KEY (StudentID, CourseID),
+Foreign KEY (StudentID) REFERENCES student(RegNumber),
+Foreign KEY (courseID) References Course(CourseID)
+);
+
+Insert into Student (RegNumber, Name,email,phone) VALUES
+(1,'Suresha','sur@gmail.com','12345');
+
+Insert into Instructor (EmpID, Name,email,Designation,phone)VALUES
+(2,'Ramesha','ram@gmail.com','Warden','9876543210');
+
+Insert Into Course(CourseID,Name , ContactHours, InstructorID)VALUES
+(3,'math',10,40);
+
+Insert into take (studentID, CourseID ,Grade)VALUES
+(1,3,'D');
